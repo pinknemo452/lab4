bac9bbb4b0b32c8bae4a4140820aa77db047ad90
pinknemo452 <mishanay38@gmail.com>
2020-10-15 17:37:09 +0300

    stable 0.2 + tests

---
+++ labFunctions.h
@@ -0,0 +1,5 @@
+#pragma once
+#include<stdio.h>
+char* uniqueChars(int slovoLength, char* slovo);
+int charequal(char a, int uniquecharsize, char* uniquechar);
+void charcount(char* word, char* string);
